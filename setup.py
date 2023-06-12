540d6f788e558c60345d73f57828565264ab9387
Chris Phillips <chris.phillips@thetradedesk.com>
2023-06-12 19:39:25 +0000

    Fix title format

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='russound_rio',
-        version='0.9.0',
+        version='0.9.1',
         packages=['russound_rio'],
         license='MIT',
         author='Chris Phillips',
